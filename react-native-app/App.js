34f243fa7518df6fd9441ce2024971b56a5817da
Alek <alek2750@gmail.com>
2018-11-14 11:00:28 +0100

    update welcome page....fuck native

--- react-native-app/App.js
+++ react-native-app/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.Headline}>Welcome page </Text>
-
+        <Text style={styles.Headline}>Welcome page</Text>
+        <ButtonHandler/>
       </View>
     );
   }
+}
+
+class ButtonHandler extends React.Component {
+  _onPressButton() {
+    Alert.alert('You tapped the button!')
+  }
+  render() {
+    return (
+      <View>
+          <Button
+            onPress={this._onPressButton}
+            title="Press here to fetch data"
+          />
+        </View>
+    );
+}
 }
 
 const styles = StyleSheet.create({
