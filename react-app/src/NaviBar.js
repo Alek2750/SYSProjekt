06ce66314bdf0576563c8a857b80a232cb066651
Johachim <joachimgul@yahoo.dk>
2018-11-14 11:13:43 +0100

    navbar 2

--- react-app/src/NaviBar.js
+++ react-app/src/NaviBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import facade from "./apiFacade";
+import './App.css';
 import {Navbar, Nav, NavItem} from "react-bootstrap";
 //Note: 'npm install react-bootstrap --save' kan forekomme nødvendigt 
 
@@ -7,16 +8,14 @@ class NaviBar extends Component{
 render() {
 return (
     <Navbar>
-    <Navbar.Header>
-        <Navbar.Brand>
-        <a href="#home">Lorem Ipsum</a>
-        </Navbar.Brand>
-    </Navbar.Header>
     <Nav>
-        <NavItem eventKey={1} href="#">
-        Login
+        <NavItem eventKey={1} href="#home">
+        Home
         </NavItem>
         <NavItem eventKey={2} href="#">
+        Login
+        </NavItem>
+        <NavItem eventKey={3} href="#">
         Logout
         </NavItem>
     </Nav>
