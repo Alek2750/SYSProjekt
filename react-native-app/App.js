d212abd2d2ab4cb5621ff0cb8394c6740a292c7d
Alek <alek2750@gmail.com>
2018-11-14 10:38:55 +0100

    native-app del

---
+++ react-native-app/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.Headline}>Welcome page </Text>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  Headline: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+});
+
