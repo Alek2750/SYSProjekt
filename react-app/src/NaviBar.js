6dd53b6461d476e3455300e20d7997d36dc8d0f7
Johachim <joachimgul@yahoo.dk>
2018-11-15 12:46:47 +0100

    'færdig' navbar

--- react-app/src/NaviBar.js
+++ react-app/src/NaviBar.js
@@ -1,28 +1,52 @@
 import React, { Component } from "react"
 import facade from "./apiFacade";
 import './App.css';
-import {Navbar, Nav, NavItem} from "react-bootstrap";
+import { Navbar, Nav, NavItem} from "react-bootstrap";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"; 
 //Note: 'npm install react-bootstrap --save' kan forekomme nødvendigt 
 
+
+const Home = () => <div><h1>Welcome to the page! Please Login to continue</h1></div>
+//Indsæt kode
+const Login = () => <div className="Login"><h1>Link jwt siden her</h1></div>;
+//Indsæt kode
+const Logout = () => <div className="Logout"><h1>Logout her </h1></div>
+//Indsæt kode
+const Data = () => <div className="Data"><h1>Data: Link til swapi eller noget her</h1></div>
 class NaviBar extends Component{
-render() {
-return (
-    <Navbar>
-    <Nav>
-        <NavItem eventKey={1} href="#home">
-        Home
-        </NavItem>
-        <NavItem eventKey={2} href="#">
-        Login
-        </NavItem>
-        <NavItem eventKey={3} href="#">
-        Logout
-        </NavItem>
-    </Nav>
-    </Navbar>
-);
-}
-}
-//Navbaren er bare en liste uden CSS styling
+    render() {
+    return (
+        <Router>
+            <Switch>
+                <div>
+        <Navbar>
+            <NavItem href="/">
+            <div>Home</div>
+            </NavItem>
+            <NavItem href="/Login">
+            <div>Login</div>
+            </NavItem>
+            <NavItem href="/Logout">
+            <div>Logout</div>
+            </NavItem>
+            <NavItem href="/Data">
+            <div>Data</div>
+            </NavItem>
+            You are logged in as: Placeholder
+        </Navbar>
+
+            <Route exact path="/" component={Home}/>
+            <Route path="/Login" component={Login}/>
+            <Route path="/Logout" component={Logout}/>
+            <Route path="/Data" component={Data}/>
+
+                </div>
+            </Switch>
+        </Router>
+    );
+    }
+    }
+    //CSS'en forbundet til App.css
+
 
 export default NaviBar;
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import Jwt from './Jwt';
 import NB from './NaviBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<NB />, document.getElementById('root'));
