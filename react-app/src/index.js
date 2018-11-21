08c8d3ff52768fa42a491ef3e4fbcbb7731d7610
Gonron <lundsgaard1997@hotmail.dk>
2018-11-21 12:31:30 +0100

    Working on Residence View

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -6,6 +6,7 @@ import AppMock from './AppMock'
 import AppClient from './AppClientPagination'
 import Jwt from './Jwt';
 import NB from './NaviBar';
+import ResidenceList from './ResidenceList';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
 
@@ -18,6 +19,7 @@ class Selector extends React.Component {
         case "a3": this.setState({ app: <AppClient /> }); break;
         case "a4": this.setState({ app: <Jwt /> }); break;
         case "a5": this.setState({ app: <NB /> }); break;
+        case "a6": this.setState({ app: <ResidenceList /> }); break;
         default: this.setState({ app: <App /> })
       }
     }
@@ -30,6 +32,7 @@ class Selector extends React.Component {
           <a href="#" id="a3"> AppClient med table </a>
           <a href="#" id="a4"> Jwt side </a>
           <a href="#" id="a5"> NaviBar side </a> 
+          <a href="#" id="a6"> Boliger </a> 
         </div>
         {this.state.app}
       </div>
