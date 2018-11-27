aa0e35c74df3550ca1fc1709e7877d2886054b5e
mortenBB <mortenbb1@gmail.com>
2018-11-27 20:00:40 +0100

    added rest endpoints for delete and edit

--- Backend/src/main/java/rest/DemoResource.java
+++ Backend/src/main/java/rest/DemoResource.java
@@ -3,16 +3,21 @@ package rest;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import entity.DataFacade;
+import entity.Housing;
 import javax.annotation.security.RolesAllowed;
+import javax.persistence.Persistence;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
 /**
@@ -44,6 +49,25 @@ public class DemoResource {
     @Path("/Housing")
     public String getAllHousing() {
         return gson.toJson(DataFacade.getInstance().getAllHousing());
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response putJson(String content, @PathParam("id") int id) {
+        Housing h = gson.fromJson(content, Housing.class);
+        h.setId(id);
+        DataFacade.getInstance().editHousing(h);        
+        return Response.ok().entity(gson.toJson(h)).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteCustomer(@PathParam("id") int id) {
+        Housing h = DataFacade.getInstance().deleteHousing(id);       
+        return Response.ok().entity(gson.toJson(h)).build();
     }
 
     @GET
