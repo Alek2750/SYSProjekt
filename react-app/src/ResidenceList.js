d7180e57d226f85fda614cc6f0419a8c9aa15291
Alek <alek2750@gmail.com>
2018-11-29 18:51:10 +0100

    remove duplicate

--- react-app/src/ResidenceList.js
+++ react-app/src/ResidenceList.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react"
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import BootstrapTable from 'react-bootstrap-table-next';
-import cellEditFactory from 'react-bootstrap-table2-editor';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
 import paginationFactory from 'react-bootstrap-table2-paginator';
