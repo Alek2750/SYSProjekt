7f8f8e265fc9efa287e34222aa04a6a06bfd6c0b
Alek <alek2750@gmail.com>
2018-12-03 10:33:09 +0100

    Laver HTML om til JS. implement All

---
+++ react-app/src/Details.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Details extends Component {
+  render() {
+    return (
+        <div>
+
+
+        </div>
+    );
+  }
+}
+
+export default Details;
