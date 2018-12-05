8b5be56d251c854db5b03eb50926830a84acb64b
Johachim <joachimgul@yahoo.dk>
2018-12-05 12:30:23 +0100

    Finpudsning af kontakt

--- react-app/src/CustomerService.js
+++ react-app/src/CustomerService.js
@@ -1,19 +1,35 @@
 import React, { Component } from "react";
-import { FaPhone, FaEnvelope } from 'react-icons/fa';
+import { FaPhone, FaEnvelope, FaClock } from 'react-icons/fa';
 class CustomerService extends Component{
     render(){
         return(
-            <div>
-                <br/>
-                <h3><FaPhone/> Ring til os på:</h3>
-                11 22 33 44
-                <br/>
-                <br/>
-                <br/>
-                <h3><FaEnvelope/> Mail:</h3>
-                Boligrunner@mail.com
+            <center>
+            <div class="col-lg-4 portfolio-item contact-landlord">
+                <div class="card">
+                    <div class="card-header"><h4>Kontakt Udlejer</h4></div>
+                    <div class="card-body">
+                        <h5 class="card-title">Telefon</h5> 
+                        <FaPhone/> 11 22 33 44
+                        <br/>
+                        <br/>
+                        <h5 class="card-title">Email</h5>
+                        <FaEnvelope/> Boligrunner@mail.com
+                        <br/>
+                        <br/>
+                        <h5 class="card-title"><FaClock/> 2+ år lejeperiode</h5>
+
+                    </div>
+                    <div class="card-footer">
+                        <button id="search" class="btn btn-primary" name="search"
+                        onclick="location.href='register.html' "type="button">
+                            <FaEnvelope/> Skriv til udlejer
+                        </button>
+                    </div>
+
+                </div>
 
             </div>
+            </center>
         )
     }
 }
