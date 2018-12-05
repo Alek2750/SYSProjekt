5f798c4e9df92cd0c2162c2b259fab8ce477322f
Johachim <joachimgul@yahoo.dk>
2018-12-05 11:27:17 +0100

    react icons

--- react-app/src/CustomerService.js
+++ react-app/src/CustomerService.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
-
+import { FaPhone, FaEnvelope } from 'react-icons/fa';
 class CustomerService extends Component{
     render(){
         return(
             <div>
-                <h3>Ring til os på:</h3>
+                <br/>
+                <h3><FaPhone/> Ring til os på:</h3>
                 11 22 33 44
                 <br/>
                 <br/>
                 <br/>
-                <h3>Mail:</h3>
+                <h3><FaEnvelope/> Mail:</h3>
                 Boligrunner@mail.com
 
             </div>
