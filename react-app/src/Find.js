d5d7c682df97bc2641a2becf5b80aae47c7ebf75
mortenBB <mortenbb1@gmail.com>
2018-12-07 13:00:57 +0100

    Fikset details knap

--- react-app/src/Find.js
+++ react-app/src/Find.js
@@ -8,117 +8,20 @@ import paginationFactory from 'react-bootstrap-table2-paginator';
 import cellEditFactory from 'react-bootstrap-table2-editor';
 import facade from "./apiFacade";
 import { Button } from 'react-bootstrap';
+import { Redirect } from 'react-router-dom'
 
 const URL = "http://localhost:8084/jwtbackend/api/info/Housing/"; //Indsæt URL for API
 const URL2 = "http://localhost:8084/jwtbackend/api/info/"; //Indsæt URL for API
 
-const btnClick = (e, id) =>{
-  console.log(e,);
-}
-
-const addButtonLancamento = (cell, row) => {
-  return (
-    <form action="/Contact">
-      <Button type="button" className="btn btn-info" onClick={(e,cell) => btnClick(e,cell)}>Detaljer</Button>
-    </form>
-  )
-}
 
 
-const labels = [{
-  dataField: 'address',
-  text: 'Adresse',
-  sort: true,
-  filter: textFilter()
-}, {
-  dataField: 'city',
-  text: 'Postnummer',
-  sort: true,
-  filter: textFilter()
-}, {
-  dataField: 'proptype',
-  text: 'Boligtype',
-  sort: true,
-  filter: textFilter()
-}, {
-  dataField: 'rooms',
-  text: 'Værelser',
-  sort: true,
-  filter: textFilter()
-},/* {
-    dataField: 'funiture',
-    text: 'Møbleret',
-    sort: true,
-    filter: textFilter()
-}, {
-    dataField: 'animals',
-    text: 'Husdyr tilladt',
-    sort: true,
-    filter: textFilter()
-},*/ {
-  dataField: 'size',
-  text: 'Størrelse',
-  sort: true,
-  filter: textFilter()
-}, {
-  dataField: 'floor',
-  text: 'Etage',
-  sort: true,
-  filter: textFilter()
-},/* {
-    dataField: 'shareable',
-    text: 'Delevenlig',
-    sort: true,
-    filter: textFilter()
-}, {
-    dataField: 'energy',
-    text: 'Energimærke',
-    sort: true,
-    filter: textFilter()
-}, {
-    dataField: 'period',
-    text: 'Lejeperiode',
-    sort: true,
-    filter: textFilter()
-},*/ {
-  dataField: 'rent',
-  text: 'Leje',
-  sort: true,
-  filter: textFilter()
-},
-{
-  dataField: 'deposit',
-  text: 'Depositum',
-  sort: true,
-  filter: textFilter()
-},
-{
-  dataField: 'openhouse',
-  text: 'Åbent hus',
-  sort: true,
-  filter: textFilter()
-},
-{
-  dataField: 'sold',
-  text: 'Solgt',
-  sort: true,
-  filter: textFilter()
-}, {
-  formatter: addButtonLancamento,
-  editable: false
-}/*, {
-    dataField: 'startperiod',
-    text: 'Overtagelsesdato',
-    sort: true,
-    filter: textFilter()
-}, {
-    dataField: 'created',
-    text: 'Oprettelsesdato',
-    sort: true,
-    filter: textFilter()
-},*/];
+
+
+
+
 
 class Find extends Component {
+
   state = { residence: [], msg: "" }
   async componentDidMount() {
 
@@ -129,7 +32,122 @@ class Find extends Component {
     this.setState({ residence, msg: "" });
   }
 
+  addButtonLancamento = () => {
+    return (
+      <form action="/Contact">
+        <Button type="submit" className="btn btn-info">Detaljer</Button>
+      </form>
+    )
+  }
+
+  handleClick = (id) => {
+    this.setState({ referrer: `/Details/${id}` });
+  }
+
   render() {
+    const labels = [{
+      dataField: 'address',
+      text: 'Adresse',
+      sort: true,
+      filter: textFilter()
+    }, {
+      dataField: 'city',
+      text: 'Postnummer',
+      sort: true,
+      filter: textFilter()
+    }, {
+      dataField: 'proptype',
+      text: 'Boligtype',
+      sort: true,
+      filter: textFilter()
+    }, {
+      dataField: 'rooms',
+      text: 'Værelser',
+      sort: true,
+      filter: textFilter()
+    },/* {
+        dataField: 'funiture',
+        text: 'Møbleret',
+        sort: true,
+        filter: textFilter()
+    }, {
+        dataField: 'animals',
+        text: 'Husdyr tilladt',
+        sort: true,
+        filter: textFilter()
+    },*/ {
+      dataField: 'size',
+      text: 'Størrelse',
+      sort: true,
+      filter: textFilter()
+    }, {
+      dataField: 'floor',
+      text: 'Etage',
+      sort: true,
+      filter: textFilter()
+    },/* {
+        dataField: 'shareable',
+        text: 'Delevenlig',
+        sort: true,
+        filter: textFilter()
+    }, {
+        dataField: 'energy',
+        text: 'Energimærke',
+        sort: true,
+        filter: textFilter()
+    }, {
+        dataField: 'period',
+        text: 'Lejeperiode',
+        sort: true,
+        filter: textFilter()
+    },*/ {
+      dataField: 'rent',
+      text: 'Leje',
+      sort: true,
+      filter: textFilter()
+    },
+    {
+      dataField: 'deposit',
+      text: 'Depositum',
+      sort: true,
+      filter: textFilter()
+    },
+    {
+      dataField: 'openhouse',
+      text: 'Åbent hus',
+      sort: true,
+      filter: textFilter()
+    },
+    {
+      dataField: 'sold',
+      text: 'Solgt',
+      sort: true,
+      filter: textFilter()
+    },
+    {
+      formatter: this.addButtonLancamento,
+      editable: false,
+      events: {
+        onClick: (e, column, columnIndex, row) => {
+          e.preventDefault();
+          console.log("hello form button, THis is the ID : " + row.id)
+          this.handleClick(row.id);
+        }
+      }
+    }/*, {
+        dataField: 'startperiod',
+        text: 'Overtagelsesdato',
+        sort: true,
+        filter: textFilter()
+    }, {
+        dataField: 'created',
+        text: 'Oprettelsesdato',
+        sort: true,
+        filter: textFilter()
+    },*/];
+
+    const { referrer } = this.state;
+    if (referrer) return <Redirect to={referrer} />;
     return (<div>
       <BootstrapTable
         striped
